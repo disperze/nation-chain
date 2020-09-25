84e0cb10b0cfcb6db59b79ea4b83a550f46502be
Giancarlos Salas <giansalex@gmail.com>
2020-09-24 19:32:24 -0500

    Add custom events

--- x/nation/types/events.go
+++ x/nation/types/events.go
@@ -2,14 +2,9 @@ package types
 
 // nation module event types
 const (
-	// TODO: Create your event types
-	// EventType<Action>    		= "action"
+	EventTypeRegisterDni = "RegisterDni"
 
-	// TODO: Create keys fo your events, the values will be derivided from the msg
-	// AttributeKeyAddress  		= "address"
-
-	// TODO: Some events may not have values for that reason you want to emit that something happened.
-	// AttributeValueDoubleSign = "double_sign"
+	AttributeDni = "dni"
 
 	AttributeValueCategory = ModuleName
 )
