4b883c3acd1a489fd03fbbd0c7eddd63c1f58be3
Giancarlos Salas <giansalex@gmail.com>
2020-09-25 21:03:33 -0500

    Add dni rest const

--- x/nation/client/rest/rest.go
+++ x/nation/client/rest/rest.go
@@ -6,6 +6,11 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 )
 
+// Params rest
+const (
+	restDni = "dni"
+)
+
 // RegisterRoutes registers nation-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	registerQueryRoutes(cliCtx, r)
